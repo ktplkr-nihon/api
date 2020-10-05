61e176dcc55b128430a51ce4555363b6a66c1db8
ktplkr-nihon <ashuengi12@gmail.com>
2020-10-05 18:00:48 +0530

    ok

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,9 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
+    console.log('hi')
     return res.status(400).json('incorrect form submission');
   }
-  console.log('hello');
   const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
       trx.insert({
