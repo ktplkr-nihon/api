0facd7d0209dce6ca64e609fc9dc543f7cdd07df
ktplkr-nihon <ashuengi12@gmail.com>
2020-10-05 15:21:40 +0530

    ok

--- controllers/register.js
+++ controllers/register.js
@@ -23,8 +23,7 @@ const handleRegister = (req, res, db, bcrypt) => {
             res.json(user[0]);
           })
       })
-      .then(trx.commit)
-      .catch(trx.rollback)
+      .then(res.json('success'))
     })
     .catch(err => res.status(400).json('unable to register'))
 }
